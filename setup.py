53f2bc807c09f026db4ae9c3e14d5ae687fe26ce
Sylvain Guieu <sylvain.guieu@gmail.com>
2022-03-10 10:26:32 +0100

    align to pydevmgr_ua 0.3.2 (bug fix in wait)

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ if sys.version_info < (3, 0, 0, 'final', 0):
 
 setup(
     name= 'pydevmgr_elt',
-    version= '0.3.3', # https://www.python.org/dev/peps/pep-0440/
+    version= '0.3.4', # https://www.python.org/dev/peps/pep-0440/
     author='Sylvain Guieu',
     author_email='sylvain.guieu@univ-grenoble-alpes.fr',
     packages=find_packages(), 
@@ -17,7 +17,7 @@ setup(
     #data_files=data_files,
     license='CeCILL Free Software License Agreement v2.1',
     long_description=open('README.md').read(),
-    install_requires=["pydevmgr_ua>=0.3.1", 
+    install_requires=["pydevmgr_ua>=0.3.2", 
 
                     'jinja2' # For templating the configuration files (see dump)
                     ],
