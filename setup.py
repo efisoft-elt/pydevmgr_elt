a04542952589f6e05982e608843710882699bb11
Sylvain Guieu <sylvain.guieu@gmail.com>
2022-03-28 19:12:23 +0200

    fix version threshol in setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 import sys
 
 # Python 3.0 or later needed
-if sys.version_info < (3, 7, 1, 'final', 0):
-    raise SystemExit('Python 3.7.1 or later is required!')
+if sys.version_info < (3, 6, 0, 'final', 0):
+    raise SystemExit('Python 3.6.0 or later is required!')
 
 
 
 setup(
     name= 'pydevmgr_elt',
-    version= '0.4.4', # https://www.python.org/dev/peps/pep-0440/
+    version= '0.4.5', # https://www.python.org/dev/peps/pep-0440/
     author='Sylvain Guieu',
     author_email='sylvain.guieu@univ-grenoble-alpes.fr',
     packages=find_packages(), 
